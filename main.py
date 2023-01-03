1772f2016309d6d24070993a6d20e77597f0dc88
JamesSharman <jamesecsharman@gmail.com>
2023-01-03 14:39:24 +0000

    Addition of toml config file and theme. Update on column spacing for photo and introduction

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ st.set_page_config(
     layout='wide'
 )
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([0.75, 1.25])
 
 with col1:
-    st.image("images/photo.jpeg", width=400)
+    st.image("images/photo.jpeg", width=400, use_column_width=True)
 
 with col2:
     st.title("James Sharman")
     content = """Hi! I am James, and welcome to my Python portfolio. I currently work as a Head of Product Solutions, managing a team of technical architects and product solution managers. 
     This portfolio demonstrates some of my technical abilities, born from my love to continue to learn and grow.  """
     st.info(content)
+
 
 content_2 = """
 You can find some of the apps I have built in Python below. Feel free to contact me!
@@ -25,7 +26,7 @@ df = pandas.read_csv("data.csv", sep=";")
 with open("data.csv") as file:
     content=file.read()
 
-st.write(content_2)
+st.subheader(content_2)
 
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
