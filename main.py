662c61d4299ddf70a3022f2b636cda8725494e72
JamesSharman <jamesecsharman@gmail.com>
2023-01-03 14:01:47 +0000

    Updated photo and added secondary content segment.

---
+++ main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(
+    layout='wide'
+)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.jpeg", width=400)
+
+with col2:
+    st.title("James Sharman")
+    content = """Hi! I am James, and welcome to my Python portfolio. I currently work as a Head of Product Solutions, managing a team of technical architects and product solution managers. 
+    This portfolio demonstrates some of my technical abilities, born from my love to continue to learn and grow.  """
+    st.info(content)
+
+content_2 = """
+You can find some of the apps I have built in Python below. Feel free to contact me!
+"""
+
+st.write(content_2)
