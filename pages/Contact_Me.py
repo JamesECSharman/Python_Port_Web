9ef730bcf7546103c19209a7a5c302057b9cd363
JamesSharman <jamesecsharman@gmail.com>
2023-01-04 11:24:30 +0000

    Send Email function added

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form('email_form', clear_on_submit=True):
+    user_email = st.text_input("Enter your email address below")
+    user_message = st.text_area("Enter your message below")
+    submit_button = st.form_submit_button("Submit")
+    if submit_button:
+        user_message = user_message + user_email
+        
