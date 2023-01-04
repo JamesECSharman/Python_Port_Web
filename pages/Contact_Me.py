c0f65514a0301bd3a2320253323387cbd5462931
JamesSharman <jamesecsharman@gmail.com>
2023-01-04 11:43:24 +0000

    Send Email added to contact_me form, and emails sending successfully.

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,11 +1,19 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
 with st.form('email_form', clear_on_submit=True):
     user_email = st.text_input("Enter your email address below")
-    user_message = st.text_area("Enter your message below")
+    raw_user_message = st.text_area("Enter your message below")
+    message = f"""\
+Subject: New Email from {user_email}
+
+From: {user_email}
+
+Message: {raw_user_message}
+"""
     submit_button = st.form_submit_button("Submit")
     if submit_button:
-        user_message = user_message + user_email
-        
+        send_email(message)
+        st.info("Your email was sent successfully.")
--- send_email.py
+++ send_email.py
@@ -1,20 +1,14 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-user_name = "jamessharmanpython@gmail.com"
-password = "kzkossvuimroilbr"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    user_name = "jamessharmanpython@gmail.com"
+    password = "kzkossvuimroilbr"
+    receiver = "jamessharmanpython@gmail.com"
+    context = ssl.create_default_context()
 
-receiver = "jamessharmanpython@gmail.com"
-context = ssl.create_default_context()
-
-message = """\
-Subject: Hi!
-How are you?
-Bye
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(user_name, password)
-    server.sendmail(user_name, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(user_name, password)
+        server.sendmail(user_name, receiver, message)
